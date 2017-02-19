85e91b96cf3726cbe5cd1a4c3a257cdaa85c3133
aleksandarcrvc <aleksandarcrvc@gmail.com>
2017-02-19 15:18:48 +0100

    adds validate middleware and use it on view

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator')
 var path = require('path');
 
 var app = express();
-//
+// Middleware logger example
 // var logger = function (req, res, next) {
 //   console.log('logging');
-//   next();
+//     next();
 // };
 //
 // app.use(logger);
@@ -19,14 +20,70 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
+app.use(function (req, res, next) {
+  res.locals.errors = null;
+  next();
+});
+
+// Express validator middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
 // Set static path
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+var users = [
+  {
+    firstName: 'Alex',
+    lastName: 'Mokac'
+  },
+  {
+    firstName: 'Tex',
+    lastName: 'Viler'
+  }
+]
+
 app.get('/', function (req, res) {
   res.render('index', {
-    title: 'Customers'
+    title: 'Customers',
+    users: users
   });
+});
+
+app.post('/users/add', function (req, res) {
+  req.checkBody('first_name', 'First name is required').notEmpty();
+  req.checkBody('last_name', 'Last name is required').notEmpty();
+
+  var errors = req.validationErrors();
+  if(errors){
+    res.render('index', {
+      title: 'Customers',
+      users: users,
+      errors: errors
+    });
+  } else {
+    var newUser = {
+      firstName: req.body.first_name,
+      lastName: req.body.last_name
+    }
+    console.log('Success');
+  }
+
 });
 
 app.listen(3000, function () {
